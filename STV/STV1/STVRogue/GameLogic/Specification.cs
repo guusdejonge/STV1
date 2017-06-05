401208a4877082a7147f241581224b86d61abac0
Tim Smit <tim.smit1997@hotmail.nl>
2017-06-05 17:15:06 +0200

    specifications 1,2,3

--- STV/STV1/STVRogue/GameLogic/Specification.cs
+++ STV/STV1/STVRogue/GameLogic/Specification.cs
@@ -20,4 +20,72 @@ namespace STVRogue.GameLogic
             verdict = verdict && G.player.HP >= 0;
         }
     }
+
+    public class Always : Specification
+    {
+        private Predicate<Game> p;
+        public Always(Predicate<Game> p) { this.p = p; }
+        public override void test(Game G) { verdict = verdict && p(G); }
+    }    public class Unless : Specification
+    {
+        private Predicate<Game> p;
+        private Predicate<Game> q;
+        public Unless(Predicate<Game> p, Predicate<Game> q) { this.p = p; this.q = q; }
+        // to keep track all past values of p && !q:
+        List<bool> history = new List<bool>();
+        public override void test(Game G)
+        {
+            bool newVerdict;
+            if (history.Count() >= 1)
+            {
+                // check if p && !q holds on the previos state:
+                bool previous = history.Last();
+                // calculate whether the previous and current state
+                // satisfies the unless property:
+                newVerdict = !previous || (previous && (p(G) || q(G)));
+            }
+            else newVerdict = true;
+            // update accumulated verdict:
+            verdict = verdict && newVerdict;
+            // push p && !q to the history:
+            history.Add(p(G) && !q(G));
+        }
+    }
+
+    public class LeadsTo : Specification
+    {
+        private Predicate<Game> p;
+        private Predicate<Game> q;
+        public LeadsTo(Predicate<Game> p, Predicate<Game> q) { this.p = p; this.q = q; }
+        // to keep track all past values of p && !q:
+        List<bool> historyP = new List<bool>();
+        List<bool> historyQ = new List<bool>();
+        public override void test(Game G)
+        {
+            if (historyP.Count() >= 1)
+            {
+                // check get last p where p was true:
+                int previousPIndex = historyP.IndexOf(historyP.Where(p=>p ==true).Last());
+                // calculate whether the previous and current state
+                // satisfies the unless property:
+                int previousQIndex = historyQ.IndexOf(historyQ.Where(q => q == true).Last());
+
+                if (previousPIndex == -1)
+                    verdict = true;
+                else
+                {
+                    if (previousQIndex > previousPIndex)
+                        verdict = true;
+                    else
+                    {
+                        verdict = false;
+                    }
+                }
+
+            }
+
+            historyP.Add(p(G));
+            historyQ.Add(q(G));
+        }
+    }
 }
